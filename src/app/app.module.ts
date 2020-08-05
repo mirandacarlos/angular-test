8622b483827e44eca410a954489e314a0ff56c65
carlos miranda <carlos@localhost.localdomain>
2020-08-05 11:44:28 +0200

    removed unnecesary package and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
-import { MdcImageListModule } from '@angular-mdc/web/image-list';
 import { MatCardModule } from "@angular/material/card";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatChipsModule } from '@angular/material/chips';
@@ -9,7 +8,6 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from "@angular/material/list";
 import { MatExpansionModule } from "@angular/material/expansion";
-import { MatButtonModule } from "@angular/material/button";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,15 +26,13 @@ import { GnomeDetailComponent } from './gnome-detail/gnome-detail.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MdcImageListModule,
     MatCardModule,
     MatDividerModule,
     MatChipsModule,
     MatGridListModule,
     MatIconModule,
     MatListModule,
-    MatExpansionModule,
-    MatButtonModule
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
